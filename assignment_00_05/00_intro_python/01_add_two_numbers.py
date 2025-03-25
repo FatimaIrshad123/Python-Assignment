1178a9430430f053045a5b5bc363d73b47b88568
FatimaIrshad123 <bibimemoona2017@gmail.com>
2025-03-25 16:23:03 +0500

    Solved assignment 1

---
+++ assignment_00_05/00_intro_python/01_add_two_numbers.py
@@ -0,0 +1,7 @@
+def add_two_numbers():
+    first_number = int(input("Enter first Number: "))
+    second_number = int(input("Enter second Number: "))
+    sum = first_number+second_number
+    print(f"The sum is: {sum}")
+
+add_two_numbers()
